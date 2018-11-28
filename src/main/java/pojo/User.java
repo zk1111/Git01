52709dc58ee84ca081a342561b4663c0f6cbe8bd
zk1111 <1813504008@qq.com>
2018-11-28 10:01:57 +0800

    user

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -14,7 +14,7 @@ public class User {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("aaabbbccc");
+		System.out.println("bbb");
 	}
 
 }
