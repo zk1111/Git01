742b977d69e6bc91580e9fa5ebb1dc7ae521fc4e
zk1111 <1813504008@qq.com>
2018-11-28 09:56:46 +0800

    pom  user

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pojo;
+
+/**
+ * @author Administrator
+ *
+ */
+public class User {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaa");
+	}
+
+}
